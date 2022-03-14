e83a6ad2a873f24d63f264974aa625d95a35e65c
braddefauw <badefauw@gmail.com>
2022-03-14 10:47:40 -0500

    calculator challenge complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a, b) => {
+    return a + b;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a, b) => {
+    return a * b;
+  });
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	result = num;
+  if(num === 0){
+    return 1;
+  }
+  while(num > 1){
+    num--;
+    result *= num;
+  }
+  return result;
 };
 
 // Do not edit below this line
