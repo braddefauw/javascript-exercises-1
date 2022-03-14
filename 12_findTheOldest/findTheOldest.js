5ba4bb13deb8bcbc299dce6d3f79ec4e4a8c5a1a
braddefauw <badefauw@gmail.com>
2022-03-14 14:55:00 -0500

    find the oldest complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(list) {
+    const getAge = function(birth, death) {
+        if (!death) {
+          death = new Date().getFullYear();
+        }
+        return death - birth;
+      };
+    const oldest = list.sort((a,b) => getAge(a.yearOfBirth, a.yearOfDeath) > getAge(b.yearOfBirth, b.yearOfDeath) ? -1:1)
+    return oldest[0];
 };
 
 // Do not edit below this line
