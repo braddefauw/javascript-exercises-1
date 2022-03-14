414e1471c9bb048ce41ee76b8e84237ae6ca14b9
braddefauw <badefauw@gmail.com>
2022-03-14 10:57:07 -0500

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    var re = /[\W_]/g;
+    var lowRegStr = str.toLowerCase().replace(re, '');
+    var reverseStr = lowRegStr.split('').reverse().join('');
+    return lowRegStr === reverseStr;
 };
 
 // Do not edit below this line
